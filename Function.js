fa5606f5de926b44ca8664999713568775c5ea50
ausaf <ausaf142@gmail.com>
2024-07-15 18:40:18 +0530

    commit

---
+++ Function.js
@@ -0,0 +1,19 @@
+function fy(){
+    console.log("fuction1")
+}
+
+
+let fun=function(){
+    console.log("fuction1")
+}
+
+
+let funky=()=>{
+    console.log("fuction1");
+}
+
+
+
+let you=(name)=>{
+    console.log("fuction1 " +name);
+};
